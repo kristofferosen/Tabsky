de49859ccee4c48e91eb982b6d457a1bb8d2086f
kristofferosen <kristofferosen@gmail.com>
2018-03-22 13:22:42 +0100

    couter

--- app/routes.js
+++ app/routes.js
@@ -6,8 +6,11 @@ module.exports = function(app) {
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
+	var x = 0;
 	app.get('*', function(req, res) {
 		res.sendfile('./public/index.html');
+		console.log("Besøk nr " + x);
+		x = x+1;
 	});
 
 };
